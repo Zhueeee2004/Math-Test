a0fc2c50e77c6cc84e148cb93b6162c2ae9a7875
Raymond Zhu <zhueeee@Raymonds-MBP.lan>
2019-10-19 17:42:58 +1300

    First step: create 10 random numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NumberSymbol } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  numbers = [6, 7, 8]
   title = 'math-test';
+
+  ngOnInit(){
+    this.numbers = this.getNumbers()
+  }
+
+  getNumbers () {
+    let numberArray = []
+    for( let i = 0; i < 10; i++) {
+      // let random = Math.random()
+      // numberArray.push(random)
+      numberArray.push(Math.random())
+    }
+    return numberArray
+  }
 }
+
